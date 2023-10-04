6682c8772bf74a6d56d17d72f1d7f1e0700eb1b3
Muhammad Fida Raditya <muhammad.120140037@student.itera.ac.id>
2023-10-04 14:18:48 +0700

    add: read, search, and update operation

--- app.py
+++ app.py
@@ -4,15 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import create_database, database_exists
 
 from sqlalchemy.sql import func #import sqlalchemy functions
+from sqlalchemy import update,create_engine,select #import sqlalchemy update function and create_engine function
+from sqlalchemy.orm import sessionmaker #import sqlalchemy session maker function
 import pymysql
 
 app=Flask(__name__)#create the flask app object with the name of the current module
 
 engine_url='mysql+pymysql://root:''@localhost/learnsqlalchemy'
-if not database_exists(engine_url):
-    create_database(engine_url)
-#Database Configuration with MySQL --> db type | username: password | path to database name    
+# if not database_exists(engine_url):
+#     create_database(engine_url)
+# #Database Configuration with MySQL --> db type | username: password | path to database name    
 app.config['SQLALCHEMY_DATABASE_URI']=engine_url
+
+engine=create_engine(engine_url)
+Session=sessionmaker(engine)
 
 db=SQLAlchemy(app)#create the database object
 
@@ -28,7 +33,15 @@ class UserInfo(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = UserInfo.query.all() #get all the data from the UserInfo table
+    search_query = 'hadehtiga'
+    
+    search_statement = select(UserInfo).where(UserInfo.username == search_query)
+    with Session() as session:
+        result = session.query(UserInfo).filter_by(username=search_query).first()
+        username = result.username if result else None # get the username from the search result
+        print(username)
+    return render_template('index.html', data=data, search_result=username)
 
 #create button function
 @app.route('/add_user', methods=['POST'])
@@ -44,6 +57,31 @@ def add_user():
          db.session.commit()
  
          return redirect(url_for('index'))
+    
+@app.route('/update_user', methods=['POST'])
+def update_user():
+    if request.method=='POST':
+        new_username=request.form['n_username']
+        old_username=request.form['o_username']
+        update_statement=(update(UserInfo)
+                            .where(UserInfo.username==old_username)
+                            .values(username=new_username)
+                            .execution_options(synchronize_session=False))
+        with Session() as session:
+            session.execute(update_statement)
+            session.commit()
+        return redirect(url_for('index'))
+    
+#Still cannot be used    
+# @app.route('/search_user', methods=['POST'])
+# def search_user():
+#     if request.method=='POST':
+#         search_query=request.form['search_query']
+#         search_statement=(select(UserInfo).where(UserInfo.username==search_query))
+#         with Session() as session:
+#             result=session.execute(search_statement)
+#         return render_template('search_result.html', search_result=result)
+
 
 if __name__=='__main__':
     app.run()
